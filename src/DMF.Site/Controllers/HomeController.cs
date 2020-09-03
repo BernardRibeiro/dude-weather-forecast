b881d49d2cfead01ad397266965b33bc5030069a
SG5843 <bernard.ribeiroengie.com>
2020-09-03 13:20:01 -0300

    creating project, updating references

---
+++ src/DMF.Site/Controllers/HomeController.cs
@@ -0,0 +1,12 @@
+﻿using System.Web.Mvc;
+
+namespace DMF.Site.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ src/DMF.Site/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="DMF.Site.MvcApplication" Language="C#" %>
